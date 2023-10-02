5a27670586e7a43c4957bfa027ca0003091c3723
Niknam1382 <niknam151412@gmail.com>
2023-10-02 11:19:46 +0330

    update project

--- resume/views.py
+++ resume/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request) :
-    return render(request, 'index.html')
+    context = {'name':'Mohammad Mahdi', 'lname':'Niknam', 'bio':'I am a student of Ali Bigdelis Django course and I just finished the fourth chapter. I follow this course through Maktabkhone website. I am interested in programming and I took the introductory and advanced Python course with Professor Jadi from Maktabkhone website and I also took the html and css course with Professor Mohammad Hossein Seyed Aghaei at Maktabkhone website.'}
+    return render(request, 'index.html', context)
